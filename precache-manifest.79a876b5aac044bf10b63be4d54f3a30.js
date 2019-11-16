66b6d8072cc97fad4cee86a2862c8b2a9bdc15c9
백계훈 <gh.back@designgj.com>
2019-11-16 19:59:55 +0900

    Updates

---
+++ precache-manifest.79a876b5aac044bf10b63be4d54f3a30.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9fd64f2a8bd02e5216e99e1a92a0b30b",
+    "url": "/movie_app_bitbong/index.html"
+  },
+  {
+    "revision": "95339d7f67af9e0ef226",
+    "url": "/movie_app_bitbong/static/css/main.e9f46f56.chunk.css"
+  },
+  {
+    "revision": "ab699226fd5d9d49bf45",
+    "url": "/movie_app_bitbong/static/js/2.1fb95e81.chunk.js"
+  },
+  {
+    "revision": "95339d7f67af9e0ef226",
+    "url": "/movie_app_bitbong/static/js/main.52f296cb.chunk.js"
+  },
+  {
+    "revision": "015b98ba89096da61634",
+    "url": "/movie_app_bitbong/static/js/runtime-main.0186e36e.js"
+  }
+]);
